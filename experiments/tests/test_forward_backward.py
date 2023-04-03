b7120523b5aea3b638e6ad5a63e5dd099794d829
Jinyang Liu <jinyang@copyrightagent.com>
2023-04-03 21:46:58 +0200

    Inference works!

--- experiments/tests/test_forward_backward.py
+++ experiments/tests/test_forward_backward.py
@@ -38,24 +38,30 @@ def test_marginal_c_when_t_is(t):
 
 def test_c_inference_single(N, t):
     for_current_t = []
-    for _ in range(N):
+    for rep in range(N):
+        print('Round', rep)
         c_sim, z_sim, x_sim = hmm.forward(n, T)
         true_c = c_sim[t]  # 0, 1, 2
         inferred_probabilities = hmm.infer_marginal_c(x_sim, t)
 
-        diff = true_c - inferred_probabilities[true_c]
+        diff = np.eye(3)[true_c] - inferred_probabilities
 
         for_current_t.append(diff)
-    return for_current_t
+    return np.array(for_current_t)
 
 
 def test_c_inference(N=100):
     diff_indicator_c_sub_inferred_c = []
     for t in range(T):
+        print('Inferring 100 rounds for', t)
         diffs = test_c_inference_single(N, t)
         diff_indicator_c_sub_inferred_c.append(diffs)
-    return diff_indicator_c_sub_inferred_c
+    return np.array(diff_indicator_c_sub_inferred_c)
 
 
-res = test_c_inference_single(10000, 3)
-print(np.mean(res))
+# res = test_c_inference_single(10000, 3)
+# print(np.mean(res))
+
+res = test_c_inference(500)
+print(np.mean(res, axis=1))
+print(np.mean(res))  # WORKS!!
