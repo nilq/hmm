b0a67ad003a50a7e9187632a0f7248ad1ae71af6
Jinyang Liu <jinyang@copyrightagent.com>
2023-04-06 17:58:04 +0200

    Add updated simulation code

--- experiments/tests/test_belief_update.py
+++ experiments/tests/test_belief_update.py
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 gamma = 0.1
 beta = 0.2
 alpha = 0.9
-rates = [1, 5]
-n = 1
-T = 2
+rates = [1, 20]
+n = 11
+T = 5
 
 # This is uppercase-gamma.
 transition_matrix = np.array(
@@ -34,47 +34,61 @@ hmmBU = HMM2(
 )
 
 
-def test_compare_with_fwbw():
+def test_compare_c_with_fwbw():
     c, z, x = hmm.forward(n, T, seed=449)
     print(c, z, x)
-    marginal_fw_bw = np.array([hmm.infer_marginal_c(x, t) for t in range(T)])
-    marginal_fw_bw_BU = hmmBU.infer_c_belief_propagation(x)
+    marginal_fw_bw, _ = np.array([hmm.infer_marginal_c(x, t) for t in range(T)])
+    marginal_fw_bw_BU, _ = hmmBU.infer_hidden_belief_propagation(x)
 
     for p1, p2 in zip(marginal_fw_bw, marginal_fw_bw_BU):
-        print('FW-BW vs BU', p1,p2)
+        print('FW-BW vs BU', p1, p2)
 
+def test_inference_absolute_error(N=100, start_seed=1):
+    absolute_error_c = []
+    absolute_error_z = []
 
-def test_c_inference2(N=100):
-    diff_indicator_c_sub_inferred_c = []
+    np.random.seed(start_seed)
     for _ in range(N):
-        print('Inferring for round', round)
+        print('Inferring for round', _)
         c, z, x = hmmBU.forward(n, T)
 
-        inferred_probs = hmmBU.infer_c_belief_propagation(x)
+        inferred_probs_c, inferred_probs_z = hmmBU.infer_hidden_belief_propagation(x)
 
-        arr = np.zeros((T, 3))
-        arr[np.arange(T), c] = 1
-        diffs = np.abs(arr - inferred_probs)
-        diff_indicator_c_sub_inferred_c.append(diffs)
-    return np.array(diff_indicator_c_sub_inferred_c)
+        # for C
+        arr_c = np.zeros(inferred_probs_c.shape)
+        arr_c[np.arange(T), c] = 1
+        ae_c = np.abs(arr_c - inferred_probs_c)
+        absolute_error_c.append(ae_c)
+
+        # for Z
+        arr_z = np.zeros(inferred_probs_z.shape)
+        arr_z[:, :, 0] = z == 0
+        arr_z[:, :, 1] = z == 1
+        ae_z = np.abs(arr_z - inferred_probs_z)
+        absolute_error_z.append(ae_z)
+    return np.array(absolute_error_c), np.array(absolute_error_z)
 
 
-# test_compare_with_fwbw()
-l = 1000
-res = test_c_inference2(l)
+def sim_and_plot(N=10000):
+    absolute_error_c, absolute_error_z = test_inference_absolute_error(N)
 
-x = []
-y = []
-# print(np.mean(res[:100, :, :]), np.mean(res[:1000, :, :]), np.mean(res[:5000, :, :]), np.mean(res))
-for i in range(1, l):
-    print(i, np.mean(res[:i, :, :]))
-    x.append(i)
-    y.append(np.mean(res[:i, :, :]))
+    c_errors = []
+    z_errors = []
+    for i in range(1, N):
+        c_errors.append((i, np.mean(absolute_error_c[:i])))
+        z_errors.append((i, np.mean(absolute_error_z[:i])))
 
-print(np.mean(res))
+    c_errors = np.array(c_errors)
+    z_errors = np.array(z_errors)
 
-sns.relplot(x=np.array(x), y=np.array(y), kind="line")
-sns.set(style="darkgrid")
-sns.set_palette("muted")
+    sns.relplot(x=c_errors[:, 0], y=c_errors[:, 1], kind="line")
+    sns.relplot(x=z_errors[:, 0], y=z_errors[:, 1], kind="line")
+    sns.set(style="darkgrid")
+    sns.set_palette("muted")
 
-plt.show()
+    plt.show()
+
+
+sim_and_plot()
+# absolute_error_c, absolute_error_z = test_inference_absolute_error(1000)
+# print(np.mean(absolute_error_c), np.mean(absolute_error_z))
