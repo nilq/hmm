32b8e443424eb4837f19172e43ec346a9e26f717
Jinyang Liu <jinyang@copyrightagent.com>
2023-04-09 14:10:07 +0200

    Learning on all given data, first one does not work

--- experiments/tests/test_on_given_data.py
+++ experiments/tests/test_on_given_data.py
@@ -9,15 +9,20 @@ def test_hard_em(filename):
 
     print(data[:, 1:])
     x = data[:, 1:]
-    (learned_gamma,
-     learned_beta,
-     learned_alpha,
-     learned_rates0,
-     learned_rates1) = hard_assignment_em(x,
-                                          initial_gamma=0.2,
-                                          initial_beta=0.2,
-                                          initial_alpha=0.7,
-                                          initial_rates=(1, 6))
+    return hard_assignment_em(
+        x,
+        initial_gamma=0.2,
+        initial_beta=0.2,
+        initial_alpha=0.7,
+        initial_rates=(1, 6)
+    )
 
 
-test_hard_em('Ex_8.csv')
+res = []
+for i in range(1, 10):
+    try:
+        res.append((i, *test_hard_em(f'Ex_{i}.csv')))
+    except Exception as e:
+        print('Couldn\'t run', i, e)
+
+print(np.array(res))
